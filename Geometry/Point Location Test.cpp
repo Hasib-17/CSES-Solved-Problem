bef842e0b0108ced7f63f844e71a840832f19bc5
Md Hasibur Rahman <107935046+Hasib-17@users.noreply.github.com>
2023-11-08 18:20:34 +0600

    Create Point Location Test.cpp

---
+++ Geometry/Point Location Test.cpp
@@ -0,0 +1,40 @@
+// بسم الله الرحمن الرحيم
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define ll long long int
+#define ul unsigned long long
+#define dec greater<int>()
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define all(x) (x).begin(), (x).end()
+#define mod 1000000007
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b)
+{
+    return (a.second < b.second);
+}
+
+void solve()
+{
+    ll x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    ll m = ((x3 - x2) * (y2 - y1)) - ((y3 - y2) * (x2 - x1));
+    if (m == 0)
+        cout << "TOUCH\n";
+    else if (m < 0)
+        cout << "LEFT\n";
+    else
+        cout << "RIGHT\n";
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+}
